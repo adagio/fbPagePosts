ab73cd70f1a2704e9458f2eced6ee0478467752e
Franco Cedillo <franco.cedillo@gmail.com>
2013-06-23 18:52:40 -0500

    new file

---
+++ sm.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type"
+content="text/html;charset=ISO-8859-1">
+    <title>Smash Magazine</title>
+
+  </head>
+  <body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=146533785539922";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+<div class="fb-like-box" data-href="https://www.facebook.com/smashmag" data-width="292" data-height="550" data-show-faces="false" data-stream="true" data-show-border="true" data-header="false"></div>
+    
+  </body>
+</html>
