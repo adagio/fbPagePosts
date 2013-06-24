18a5007066ed26a15558f8967710f45e97b0f013
Franco Cedillo <franco.cedillo@gmail.com>
2013-06-23 23:01:45 -0500

    sm ready elements

--- sm.php
+++ sm.php
@@ -18,7 +18,32 @@ content="text/html;charset=ISO-8859-1">
 }(document, 'script', 'facebook-jssdk'));</script>
 
 
+<div class="fb-like" data-href="http://dctfb.com/sm.php" data-send="false" data-width="450" data-show-faces="true"></div>
+
+<div class="fb-send" data-href="http://dctfb.com/sm.php"></div>
+
+<img src="https://graph.facebook.com/smashmag/picture " />
+
+<?php
+/***
+ https://graph.facebook.com/smashmag
+***/
+?>
+
 <div class="fb-like-box" data-href="https://www.facebook.com/smashmag" data-width="292" data-height="550" data-show-faces="false" data-stream="true" data-show-border="true" data-header="false"></div>
-    
+
+<div class="fb-comments" data-href="http://dctfb.com/smashmag.html" data-width="470" data-num-posts="10"></div>
+
+
+<a href="#" 
+  onclick="
+    window.open(
+      'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 
+      'fb-share-dialog', 
+      'width=626,height=436'); 
+    return false;">
+  Share on Facebook
+</a>
+
   </body>
 </html>
